14116b695e454f2afc221b56ec1128beff976d02
Devin Hurley <snowmiser111@mac.com>
2015-02-23 23:12:36 -0500

    I think I got RandomGraph working..

--- Ch2/ex2-4/RandomGraph.py
+++ Ch2/ex2-4/RandomGraph.py
@@ -8,10 +8,19 @@ class RandomGraph(Graph):
     """
     Erdos-Renyi model for a random graph
     """
-    def add_random_edges(p):
+    def add_random_edges(self,p):
         for vertex in self.vertices():
             for vert in self.vertices():
-            ranNum = random.random()
-            if ranNum < p and vertex != vert:
-                self.add_edge(Edge(vertex,vert))
+                ranNum = random.random()
+                if ranNum < p and vertex != vert:
+                    self.add_edge(Edge(vertex,vert))
+
+
+
+if __name__ == '__main__':
+    v_s = ['a','b','c','d','e']
+    RG = RandomGraph(Graph([Vertex(item) for item in v_s]))
+    RG.add_random_edges(.5)
+    print len(RG.edges())
+        
 
